2cd7752c7d18a543b133b89b6e82ab5f5297c100
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-05-23 22:26:03 -0300

    2

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#323001';
+
+export default {
+  tintColor,
+  tabIconDefault: '#323001',
+  tabIconSelected: tintColor,
+  tabBar: '#323001',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#ff323001',
+};
---
+++ screens/HomeScreen.js
@@ -0,0 +1,181 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        <View style={styles.getStartedContainer}>
+          <DevelopmentModeNotice />
+
+          <Text style={styles.getStartedText}>Welcome to my Personal Music App.  Version 0.0.1</Text>
+
+          
+         
+
+          <Text style={styles.getStartedText}>
+            Give me a feedback:
+          </Text>
+        </View>
+        <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+            
+          
+          
+          </View>
+        
+
+        <View style={styles.helpContainer}>
+          <TouchableOpacity onPress={() => WebBrowser.openBrowserAsync('https://github.com/jowpurcinelli')} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>Click and take a look at my GitHub</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>This is a tab bar.</Text>
+
+        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+function DevelopmentModeNotice() {
+  if (__DEV__) {
+    return (
+      <Text style={styles.developmentModeText}>
+        Development mode is enabled
+      </Text>
+    );
+  } else {
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode: your app will run at full speed.
+      </Text>
+    );
+  }
+}
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#323001',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 14,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 10,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 90,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 1,
+    marginLeft: 0,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(100,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    position: 'relative',
+    textAlign: 'center',
+    
+  },
+  getStartedText: {
+    fontSize: 14,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        borderRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#232009',
+    paddingVertical: 30,
+    borderRadius: 12,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    borderRadius: 10,
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#7d7803',
+  },
+});
