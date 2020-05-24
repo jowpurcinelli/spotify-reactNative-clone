f17837d2249fc50a9a1026a5477cafbec491deab
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-05-23 22:29:01 -0300

    dale

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,9 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import TabBarIcon from '../src/components/TabBarIcon';
+import HomeScreen from '../src/screens/HomeScreen';
+import LinksScreen from '../src/screens/LinksScreen';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
